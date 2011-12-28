d40fcbb51435ccf55a0ee8fec609a7b85cb8c91d
Oskar Werkelin Ahlin <osk@pwn.(none)>
2011-12-28 20:57:48 +0100

    More fixes.

--- HumanPlayer.hh
+++ HumanPlayer.hh
@@ -1,3 +1,8 @@
+/**
+ * HumanPlayer.hh
+ * Author: Oskar Werkelin Ahlin
+ **/
+
 #ifndef HUMANPLAYER_HH
 #define HUMANPLAYER_HH
 
@@ -6,8 +11,18 @@
 
 #include <cstdio>
 
+/** 
+ * Simple class that makes moves by reading from stdin. Also
+ * checks that the given moves are valid. 
+ */
 class HumanPlayer: public Player {
     public:
+        /** 
+         * Get a move. Read from stdin.
+         *
+         * @param Board current board
+         * @return Desired move.
+         **/
         int getMove(Board& board) {
             int move;
             for (;;) {
--- Player.hh
+++ Player.hh
@@ -1,11 +1,30 @@
+/**
+ * Player.hh
+ * Author: Oskar Werkelin Ahlin
+ **/
+
 #ifndef PLAYER_HH
 #define PLAYER_HH
 
 #include "Board.hh"
 
+/**
+ * Player is an abstract class which represents a player objects. All
+ * inheriting classes must implement getMove().
+ **/
 class Player {
     public:
+        /*
+         * Id of the player (0/1).
+         */
         int playerid;
+
+        /**
+         * Returns a move.
+         *
+         * @param Board Current board.
+         * @return Move Decided move.
+         */
         virtual int getMove(Board& board)=0;
 };
 
